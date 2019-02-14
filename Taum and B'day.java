76bd9e913f59c487e67a59c0f5c51ef86eb01085
Pedja Janjic <44039369+Janja7@users.noreply.github.com>
2019-02-14 14:09:17 +0100

    Add files via upload

---
+++ Taum and B'day.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the taumBday function below.
+    static long taumBday(int b, int w, int bc, int wc, int z) {
+        long result = 0;
+
+        long minBlack = Math.min(bc, wc + z);
+        long minWhite = Math.min(wc, bc + z); 
+
+        result = (b * minBlack) + (w * minWhite);
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int t = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int tItr = 0; tItr < t; tItr++) {
+            String[] bw = scanner.nextLine().split(" ");
+
+            int b = Integer.parseInt(bw[0]);
+
+            int w = Integer.parseInt(bw[1]);
+
+            String[] bcWcz = scanner.nextLine().split(" ");
+
+            int bc = Integer.parseInt(bcWcz[0]);
+
+            int wc = Integer.parseInt(bcWcz[1]);
+
+            int z = Integer.parseInt(bcWcz[2]);
+
+            long result = taumBday(b, w, bc, wc, z);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
